3d5efa2c12000f58d9dcb1fd5e62c1208133f21b
sabrina <sabrinaabrodi20@gmail.com>
2023-01-24 18:07:54 +0100

    feat: stores imgs

---
+++ src/stores.js
@@ -0,0 +1,20 @@
+export const imgs = [
+	{
+		img: 'https://twinfinite.net/wp-content/uploads/2022/05/Nezuko-Demon-Slayer-Characters.jpg?resize=1000%2C563'
+	},
+	{
+		img: 'https://staticg.sportskeeda.com/editor/2022/01/a750f-16419270064851.png'
+	},
+	{
+		img: 'https://fictionhorizon.com/wp-content/uploads/2022/07/Inosuke_waking_up.webp'
+	},
+	{
+		img: 'https://www.siliconera.com/wp-content/uploads/2021/10/demonslayer_tanjiro.jpg?resize=1200%2C675'
+	},
+	{
+		img: 'http://www.zwinnieyap.com/uploads/3/1/8/4/31842617/uzui-tengen-18_orig.jpg'
+	},
+	{
+		img: 'https://anime.atsit.in/de/wp-content/uploads/2022/09/zenitsu-agatsuma-von-demon-slayer-erhalt-geburtstagstrailer.jpg'
+	}
+];
